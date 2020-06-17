de70eeba9e125dd8534cfe191a8be8fd7937a049
sunyinglong <sunyinglong@rongcloud.cn>
2020-06-17 10:26:14 +0800

    调整解析Herewhite白板创建接口响应的参数

--- src/main/java/cn/rongcloud/pojo/HereWhiteApiResultInfo.java
+++ src/main/java/cn/rongcloud/pojo/HereWhiteApiResultInfo.java
@@ -17,41 +17,14 @@ public class HereWhiteApiResultInfo {
     @Data
     public static class HereWhiteMsg {
         private HereWhiteRoom room;
-        private Hare hare;
         private String roomToken;
         private String code;
 
     }
 
     @Data
-    public static class Hare {
-        private String uuid;
-        private String appIdentifier;
-        private String appVersion;
-        private String appHash;
-        private String akkoVersion;
-        private String teamId;
-        private String mode;
-        private String region;
-        private Boolean isBan;
-        private Long createdAt;
-        private Long updatedAt;
-        private Integer usersMaxCount;
-        private Long survivalDuration;
-    }
-
-    @Data
     public static class HereWhiteRoom {
-        private Integer id;
         private String name;
-        private Integer limit;
-        private Integer teamId;
-        private Integer adminId;
-        private String mode;
-        private String template;
-        private String region;
         private String uuid;
-        private String updatedAt;
-        private String createdAt;
     }
 }
